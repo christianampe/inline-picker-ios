87724718adc901817beccf0d0a9bb7bbe05dcc75
christianampe <christianampe.work@gmail.com>
2018-08-04 16:26:55 -0700

    set up initalizers

--- FlatInlinePicker/Classes/FlatInlinePicker.swift
+++ FlatInlinePicker/Classes/FlatInlinePicker.swift
@@ -10,6 +10,10 @@ import Foundation
 @IBDesignable
 open class FlatInlinePicker: UIView {
     
+    // MARK: Storage
+    open var delegate: FlatInlinePickerDelegate?
+    open var dataSource: FlatInlinePickerDataSource?
+    
     // MARK: IBInspectables
     @IBInspectable
     open var interitemSpacing: CGFloat = 2.0
@@ -21,5 +25,32 @@ open class FlatInlinePicker: UIView {
     open var padding: CGSize = .zero
     
     
+    // MARK: Designable Initalizers
+    public convenience init() {
+        
+        self.init(frame: CGRect.zero)
+    }
     
+    public override convenience init(frame: CGRect) {
+        
+        self.init(frame: frame)
+    }
+    
+    // MARK: Programmatic Initalizer
+    public init(_ frame: CGRect,
+                config: FlatInlinePickerConfig,
+                delegate: FlatInlinePickerDelegate?,
+                dataSource: FlatInlinePickerDataSource?) {
+        
+        self.delegate = delegate
+        self.dataSource = dataSource
+        
+        super.init(frame: frame)
+    }
+    
+    // MARK: Storyboard Initalizer
+    public required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
 }
